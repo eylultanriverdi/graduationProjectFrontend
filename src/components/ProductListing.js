b0eb98ece87b4346bb3b633074a976fd20d458df
eylultanriverdi <eylulrabiatanriverdi@gmail.com>
2023-06-02 03:53:23 +0300

    pagination made

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,62 +1,102 @@
-import React, { useEffect } from 'react';
-import { connect } from "react-redux";
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { setProducts } from '../redux/actions/productActions';
 import axios from 'axios';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const ProductListing = (props) => {
   const { allProducts } = props;
-  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(10); // Her sayfada kaç ürün gösterileceği
 
   const fetchProducts = async () => {
     try {
-      const resp = await axios.get("http://localhost:3001/products");
+      const resp = await axios.get(`http://localhost:3001/products?page=${currentPage}&limit=${productsPerPage}`);
       if (resp && resp.status === 200) {
-        const decodedProducts = resp.data.map((product) => {
+        const { pagination, products } = resp.data;
+        const { totalPages } = pagination;
+  
+        const decodedProducts = products.map((product) => {
           const base64Image = `data:image/jpeg;base64,${product.productImage}`;
           return {
             ...product,
             productImage: base64Image
           };
         });
-        
-        
-        dispatch(setProducts(decodedProducts));
+  
+        props.setProducts(decodedProducts);
       }
     } catch (error) {
-      console.log("Error:", error);
+      console.log('Error:', error);
     }
   };
+  
+
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [currentPage]);
 
+  // Şu anki sayfadaki ürünleri al
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = allProducts.slice(indexOfFirstProduct, indexOfLastProduct);
 
-  const renderList = allProducts && allProducts.map((product) => {
-    const { productId, productName, description, productImage, carbohydrateValue, glutenFree, glutenValue, ketogenicDiet, oilValue, proteinValue, saltFree ,calorieValue} = product;
+  // Sayfa değişikliğini işle
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
+
+  const renderList = allProducts.map((product) => {
+    const { productId, productName, description, productImage, carbohydrateValue, glutenFree, glutenValue, ketogenicDiet, oilValue, proteinValue, saltFree, calorieValue } = product;
   
     return (
-      <Link to={`/product/${productId}`} style={{ color: "#9c27b0" }} key={productId}>
-        <Paper elevation={10} style={{ marginTop: "50px", marginBottom: "80px" }}>
+      <Link to={`/product/${productId}`} style={{ color: '#9c27b0' }} key={productId}>
+        <Paper elevation={10} style={{ marginTop: '50px', marginBottom: '80px' }}>
           <Grid container spacing={2}>
+            {/* Ürün görseli */}
             <Grid item xs={12} sm={4}>
-              <img src={productImage} alt={description} align="center" style={{ width: "300px", height: "350px", marginBottom: "50px", marginLeft: "10px" }} />
+              <img
+                src={productImage}
+                alt={description}
+                align="center"
+                style={{ width: '300px', height: '350px', marginBottom: '50px', marginLeft: '10px' }}
+              />
             </Grid>
-            <Grid item xs={12} sm={4} style={{ color: "#9c27b0" }}>
-              <Typography variant="h4" component="h2">{productName}</Typography>
-              <Typography variant="h4" component="h2">{proteinValue}</Typography>
-              <Typography variant="h4" component="h2">{carbohydrateValue}</Typography>
-              <Typography variant="h4" component="h2">{oilValue}</Typography>
-              <Typography variant="h4" component="h2">{glutenValue}</Typography>
-              <Typography variant="h4" component="h2">{glutenFree}</Typography>
-              <Typography variant="h4" component="h2">{ketogenicDiet}</Typography>
-              <Typography variant="h4" component="h2">{saltFree}</Typography>
-              <Typography variant="h4" component="h2">{calorieValue}</Typography>
+            {/* Ürün bilgileri */}
+            <Grid item xs={12} sm={4} style={{ color: '#9c27b0' }}>
+              <Typography variant="h4" component="h2">
+                {productName}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {proteinValue}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {carbohydrateValue}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {oilValue}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {glutenValue}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {glutenFree}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {ketogenicDiet}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {saltFree}
+              </Typography>
+              <Typography variant="h4" component="h2">
+                {calorieValue}
+              </Typography>
             </Grid>
           </Grid>
         </Paper>
@@ -65,10 +105,16 @@ const ProductListing = (props) => {
   });
   
 
+  // Toplam sayfa sayısını hesapla
+  const pageNumbers = Math.ceil(allProducts.length / productsPerPage);
+
   return (
     <div>
-      <h1 style={{ color: "#9c27b0" }}>Product Listing</h1>
+      <h1 style={{ color: '#9c27b0' }}>Product Listing</h1>
       {renderList}
+      <Stack spacing={2} style={{ marginTop: '20px' }}>
+        <Pagination count={8} color="secondary" page={currentPage} onChange={handlePageChange} />
+      </Stack>
     </div>
   );
 };
@@ -77,6 +123,8 @@ const mapStateToProps = (state) => ({
   allProducts: state.allProducts.products
 });
 
-const mapDispatchToProps = () => ({});
+const mapDispatchToProps = {
+  setProducts
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductListing);
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,12 +1,12 @@
 import { ActionTypes } from "../contants/action-types";
 const initialState = {
-    products: []
+    products: [],
 }
 
 export const productReducer = (state = initialState, { type, payload }) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return { ...state, products: payload }
+            return { ...state, products: payload}
         default:
             return state
     }
